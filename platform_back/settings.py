49b0e5362f16c41e5b792dc813dbb28092cae03a
Amoki <hugo.duroux@gmail.com>
2018-08-10 18:01:27 +0200

    fix context processor

--- platform_back/settings.py
+++ platform_back/settings.py
@@ -83,7 +83,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "login.context_processors.oidc_client_id",
+                "login.context_processors.oidc_settings",
             ]
         },
     }
