f2f93f4c063a262eccc80ff8e28b5ad8884c150e
Amoki <hugo.duroux@gmail.com>
2018-08-27 16:22:58 +0200

    add header support

--- platform_back/settings.py
+++ platform_back/settings.py
@@ -36,6 +36,8 @@ else:
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 # Application definition
 
@@ -44,6 +46,7 @@ INSTALLED_APPS = [
     "login",
     "rest_framework",
     "drf_yasg",
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "mozilla_django_oidc",  # Load after auth
@@ -56,6 +59,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
