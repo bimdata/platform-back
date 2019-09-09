0c43610e604914d12191ff487025271082ab84db
Amoki <hugo.duroux@gmail.com>
2019-09-09 18:15:47 +0200

    fix OIDC_OP_ISSUER setting

--- platform_back/settings.py
+++ platform_back/settings.py
@@ -148,11 +148,6 @@ PLATFORM_BACK_URL = os.environ.get("PLATFORM_BACK_URL", "http://127.0.0.1:8082")
 MANDRILL_KEY = os.environ.get("MANDRILL_KEY", False)
 MANDRILL_TEST_KEY = os.environ.get("MANDRILL_TEST_KEY", False)
 
-ACTIVE_CAMPAIGN_EVENT_KEY = os.environ.get("ACTIVE_CAMPAIGN_EVENT_KEY", False)
-ACTIVE_CAMPAIGN_KEY = os.environ.get("ACTIVE_CAMPAIGN_KEY", False)
-ACTIVE_CAMPAIGN_URL = os.environ.get("ACTIVE_CAMPAIGN_URL", False)
-ACTIVE_CAMPAIGN_APP_LIST_ID = os.environ.get("ACTIVE_CAMPAIGN_APP_LIST_ID", False)
-
 WEBHOOKS_SECRET = os.environ.get("WEBHOOKS_SECRET", "123")
 
 # Internationalization
@@ -284,7 +279,7 @@ OIDC_AUTH = {
     # Specify OpenID Connect endpoint. Configuration will be
     # automatically done based on the discovery document found
     # at <endpoint>/.well-known/openid-configuration
-    "OIDC_ENDPOINT": os.environ.get("OIDC_OP_ISSUER", "http://localhost:8000"),
+    "OIDC_ENDPOINT": OIDC_OP_ISSUER,
     # Accepted audiences the ID Tokens can be issued to
     "OIDC_AUDIENCES": ("account",),
     # (Optional) Function that resolves id_token into user.
