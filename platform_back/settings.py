2245940852639368f1d944a69dbdf5400c503f05
Amoki <hugo.duroux@gmail.com>
2019-10-09 16:15:20 +0200

    tokens can be valid up to 1 hour now

--- platform_back/settings.py
+++ platform_back/settings.py
@@ -176,7 +176,7 @@ REST_FRAMEWORK = {
 
 
 # LOGGING EMAIL
-ADMINS = [('infra', 'infra@bimdata.io')]
+ADMINS = [("infra", "infra@bimdata.io")]
 SERVER_EMAIL = "bug@bimdata.io"
 EMAIL_HOST = "smtp.mandrillapp.com"
 EMAIL_HOST_PASSWORD = os.environ.get("MANDRILL_SMTP_KEY", False)
@@ -291,7 +291,7 @@ OIDC_AUTH = {
     "OIDC_RESOLVE_USER_FUNCTION": "user.auth.get_user_by_id",
     # (Optional) Number of seconds in the past valid tokens can be
     # issued (default 600)
-    "OIDC_LEEWAY": 600,
+    "OIDC_LEEWAY": 60 * 60,  # 60 minutes
     # (Optional) Time before signing keys will be refreshed (default 24 hrs)
     "OIDC_JWKS_EXPIRATION_TIME": 24 * 60 * 60,
     # (Optional) Time before bearer token validity is verified again (default 10 minutes)
