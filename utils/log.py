d5e7851d859a66c9bbc14fee23b287a15f89d14a
Antoine Mineau <antoine@bimdata.io>
2021-01-11 18:09:39 +0100

    add env to fluent log

--- utils/log.py
+++ utils/log.py
@@ -18,6 +18,13 @@ logger.addHandler(logHandler)
 def log_user_connect(func):
     def wrapper(*args, **kwargs):
         user = func(*args, **kwargs)
-        logger.info({"email": user.email, "action": "connect_to_platform", "message": f"{user.email} has just logged on to the platform"})
+        logger.info(
+            {
+                "env": settings.ENV,
+                "email": user.email,
+                "action": "connect_to_platform",
+                "message": f"{user.email} has just logged on to the platform",
+            }
+        )
 
     return wrapper
