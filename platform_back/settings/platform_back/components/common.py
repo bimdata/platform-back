3b4288a1f57fe729f7c8f3a7fd8c0e3b766c0488
Antoine Mineau <antoine@bimdata.io>
2021-11-17 16:56:36 +0100

    remove REQUESTS_CA_BUNDLE setting

--- platform_back/settings/platform_back/components/common.py
+++ platform_back/settings/platform_back/components/common.py
@@ -15,7 +15,6 @@ PLATFORM_URL = env("PLATFORM_URL", default="")
 PLATFORM_BACK_URL = env("PLATFORM_BACK_URL", default="")
 
 WEBHOOKS_SECRET = env("WEBHOOKS_SECRET", default="")
-REQUESTS_CA_BUNDLE = env("REQUESTS_CA_BUNDLE", default="")
 MASTER_TOKEN = env("MASTER_TOKEN", default="")
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
