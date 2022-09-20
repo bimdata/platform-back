e7bd4bc8efb3fc56efa1565a0a49ea6eb8be3326
Antoine Mineau <antoine@bimdata.io>
2022-09-20 16:43:14 +0200

    refacto email env

--- platform_back/settings/platform_back/components/email.py
+++ platform_back/settings/platform_back/components/email.py
@@ -2,14 +2,16 @@ from platform_back.settings.environ import env
 
 DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default="no-reply@bimdata.io")
 
-SERVER_EMAIL = env("DEBUG_MAIL_TO", default="bug@bimdata.io")
+SERVER_EMAIL = env("SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 
 if EMAIL_HOST := env("SMTP_HOST", default=None):
-    EMAIL_PORT = env("SMTP_PORT")
-    EMAIL_HOST_USER = env("SMTP_USER")
-    EMAIL_HOST_PASSWORD = env("SMTP_PASS")
-    EMAIL_USE_TLS = env.bool("SMTP_USE_TLS")
+    EMAIL_PORT = env("SMTP_PORT", default=None)
+    EMAIL_HOST_USER = env("SMTP_USER", default=None)
+    EMAIL_HOST_PASSWORD = env("SMTP_PASS", default=None)
+    EMAIL_USE_TLS = env.bool("SMTP_USE_TLS", default=True)
 
-ADMINS = [("infra", "infra@bimdata.io")]
+DEBUG_MAIL_TO = env("DEBUG_MAIL_TO", default="infra@bimdata.io")
+
+ADMINS = [("Admin Platform-back", DEBUG_MAIL_TO)]
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
