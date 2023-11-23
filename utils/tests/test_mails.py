b48e7de8df8bad1025595083795696a0b5e5bcfa
NicolasRichel <nicolas@bimdata.io>
2023-11-23 15:59:36 +0100

    feat: customize welcome email subject with user name

--- utils/tests/test_mails.py
+++ utils/tests/test_mails.py
@@ -1,14 +1,19 @@
 from django.core import mail
 from django.test import TestCase
-from utils import mailer
-from parameterized import parameterized
 from django.utils.translation import activate
+from parameterized import parameterized
+
+from utils import mailer
 
 
 class EmailTest(TestCase):
     @parameterized.expand(
         [
-            ("mailing-welcome", {}, "Bienvenue sur la plateforme BIMData.io"),
+            (
+                "mailing-welcome",
+                {"user_name": "Jane"},
+                "Bienvenue sur la plateforme BIMData.io Jane",
+            ),
         ]
     )
     def test_send_email_invitation_with_django(
