478c80058b07f2b997884de49005c44977eb8a17
Amoki <hugo@bimdata.io>
2025-03-19 18:08:09 +0100

    fix tests

--- utils/tests/test_mails.py
+++ utils/tests/test_mails.py
@@ -1,25 +1,15 @@
 from django.core import mail
 from django.test import TestCase
-from parameterized import parameterized
 
-from utils import mailer
+from user.models import User
 
 
 class EmailTest(TestCase):
-    @parameterized.expand(
-        [
-            (
-                "mailing-welcome",
-                {},
-                "Bienvenue sur la plateforme BIMData.io",
-            ),
-        ]
-    )
-    def test_send_email_invitation_with_django(
-        self, template_name, context, subject_expected
-    ):
+    def test_send_email_invitation_with_django(self):
         # Send message.
-        mailer.send_mail(template_name, context, [{"email": "test@bimdata.io"}], "FR")
+        User.objects.create(email="test@bimdata.io", language="fr")
 
         self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].subject, subject_expected)
+        self.assertEqual(
+            mail.outbox[0].subject, "Bienvenue sur la plateforme BIMData.io"
+        )
